dd8d8cb2b460af3b9ed61d23aa76abc99c692522
Janghyun Son <jhson989@gmail.com>
2023-04-28 17:35:26 +0900

    Change module's name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module server-clients
+module goServerAndClients
 
 go 1.20
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"goServerAndClients/client"
+	"goServerAndClients/server"
 	"runtime"
-	"server-clients/client"
-	"server-clients/server"
 	"sync"
 )
 
